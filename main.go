efdf4b01752bb5e5c346de0d9d4722dedf7f6811
Shubham Jain <shubhamjain2908@gmail.com>
2020-09-13 18:33:23 +0530

    added doSimple function

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+
+	simplepb "github.com/shubhamjain2908/protobuf-example-go/src/simple"
 )
 
 func main() {
 	fmt.Println("Hello Go!")
+
+	doSimple()
+}
+
+func doSimple() {
+	sm := simplepb.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My simple Message",
+		SampleList: []int32{1, 2, 7, 8},
+	}
+
+	fmt.Println(sm)
+
+	sm.Name = "I renamed you"
+
+	fmt.Println("Id => ", sm.GetId())
+	fmt.Println("Name => ", sm.GetName())
 }
